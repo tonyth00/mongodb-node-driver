a1bd35e3139550cdaec1826a6b7d3c2aae3dff90
Tony Huang <tonyth00@gmail.com>
2020-07-06 15:45:54 -0700

    remove credentials

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const mongoDbUrl =
-  'mongodb+srv://tonyth00:well3192@cluster0.nfgvj.mongodb.net/shop?retryWrites=true&w=majority';
+  'mongodb+srv://<username>:<password>@cluster0.nfgvj.mongodb.net/shop?retryWrites=true&w=majority';
 let _db;
 const initDb = (callback) => {
   if (_db) {
